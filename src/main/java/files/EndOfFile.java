9d31c1cc506e1163efcc8be875d74da5bd97b08c
adrianromanski <adrianromanski@icloud.com>
2020-07-21 17:04:41 +0200

    Added Exercise - EndOfFile

---
+++ src/main/java/files/EndOfFile.java
@@ -0,0 +1,19 @@
+package files;
+
+import java.util.Scanner;
+//The challenge here is to read n lines of input until you reach EOF, then number and print all n lines of content.
+public class EndOfFile {
+
+    public static void main(String[] args) {
+
+        int count = 0;
+        StringBuilder sb = new StringBuilder();
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()) {
+            count+=1;
+            String s = sc.nextLine();
+            sb.append(count + " " + s + "\n");
+        }
+        System.out.println(sb.toString());
+    }
+}
