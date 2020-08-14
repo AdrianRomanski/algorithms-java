61cfa991e14b6d1709cad80491b22a6e345f673d
adrianromanski <adrianromanski@icloud.com>
2020-08-14 10:49:49 +0200

    Added 30daysOfCode - Calculator

---
+++ src/main/java/days30_of_code/CalculatorTest.java
@@ -0,0 +1,38 @@
+package days30_of_code;
+
+import java.util.Scanner;
+
+public class CalculatorTest {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        while (t-- > 0) {
+
+            int n = in.nextInt();
+            int p = in.nextInt();
+            Calculator myCalculator = new Calculator();
+            try {
+                int ans = myCalculator.power(n, p);
+                System.out.println(ans);
+            }
+            catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        in.close();
+    }
+}
+
+class Calculator {
+
+    public static int power(int y, int x) {
+
+        if(y < 0 || x < 0) {
+            throw new RuntimeException("n and p should be non-negative");
+        } else {
+            return (int) Math.pow(y, x);
+        }
+    }
+}
