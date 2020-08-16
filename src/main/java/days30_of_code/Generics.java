d8b9b95880d9fbab0dde5b6348b0c55ac1e83219
adrianromanski <adrianromanski@icloud.com>
2020-08-16 14:39:49 +0200

    Added 30daysOfCode - Generics

---
+++ src/main/java/days30_of_code/Generics.java
@@ -0,0 +1,11 @@
+package days30_of_code;
+
+public class Generics<T> {
+
+
+    public void printArray (T[] array) {
+        for(T t: array) {
+            System.out.println(t);
+        }
+    }
+}
