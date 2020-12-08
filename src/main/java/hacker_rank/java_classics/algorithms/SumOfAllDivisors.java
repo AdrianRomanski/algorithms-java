7614cb6f14196ad043768d7a1bb6600ca8923338
adrianromanski <adrianromanski@icloud.com>
2020-12-08 12:24:39 +0100

    Added HackerRank SumOfDivisors

---
+++ src/main/java/hacker_rank/java_classics/algorithms/SumOfAllDivisors.java
@@ -0,0 +1,20 @@
+package hacker_rank.java_classics.algorithms;
+
+public class SumOfAllDivisors {
+
+    public static void main(String[] args) {
+
+        System.out.println(sumOfDivisors(6));
+
+    }
+
+    static int sumOfDivisors(int n) {
+        int sum = 0;
+        for(int i = 1; i <= n; i++) {
+            if(n % i == 0) {
+                sum+=i;
+            }
+        }
+        return sum;
+    }
+}
